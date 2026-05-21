621d00538311e2a16a226aef8876a5a4880fc2c5
odioski <o.daniels.sr@outlook.com>
2026-05-20 21:18:41 -0500

    ...

--- package-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-PACKAGE_NAME="K-Splash.plasmoid"
-
-rm -f "$PACKAGE_NAME"
-zip -r "$PACKAGE_NAME" metadata.json contents -x '.git' '.git/*' '.codex' '.codex/*' 'contents/config/local.json'
-
-echo "Built $PACKAGE_NAME"
